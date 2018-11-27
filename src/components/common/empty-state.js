b44a14b5a05f222b30bbec80f835e4b2b25b0afd
Sivadass N <nsivadass@gmail.com>
2018-11-27 23:59:19 +0530

    Updated no data component

--- src/components/common/empty-state.js
+++ src/components/common/empty-state.js
@@ -3,7 +3,7 @@ import React from "react";
 const EmptyState = ({ title, message }) => {
   return (
     <div className="empty-state">
-      <h1>{title ? title : "Sorry, no data!"}</h1>
+      <h1>{title ? title : "Sorry, no data available!"}</h1>
       <p>{message ? message : "Please add some data!"}</p>
     </div>
   );
